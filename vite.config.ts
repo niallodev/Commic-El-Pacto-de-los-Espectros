1cb0ab0c0cb6fb99a592f599c064dc78636c3168
niallodev <nloorbazurto@gmail.com>
2024-12-22 16:53:03 -0500

    Configuración para la publicación en GithubPage

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Commic-El-Pacto-de-los-EspectrosCommic-El-Pacto-de-los-Espectros/', // Reemplaza con el nombre de tu repositorio
   plugins: [react()],
+  build: {
+    outDir: 'build', // Cambia el nombre de la carpeta generada
+  },
 })
